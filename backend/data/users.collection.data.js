175fd377e9dc404fad0d335668b78dfe0e7af178
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-12-28 17:24:39 +0200

    chore: add .prettierrc configs

--- backend/data/users.collection.data.js
+++ backend/data/users.collection.data.js
@@ -1,18 +1,18 @@
 export const usersCollectionData = [
   {
     id: 20904,
-    name: "John Doe",
-    email: "john@example.com",
-    role: "customer",
-    password: "1234",
-    avatar: "avatar.jpg",
+    name: 'John Doe',
+    email: 'john@example.com',
+    role: 'customer',
+    password: '1234',
+    avatar: 'avatar.jpg',
   },
   {
     id: 20910,
-    name: "Taylor Doe",
-    email: "taylor@example.com",
-    role: "seller",
-    password: "1234",
-    avatar: "avatar.jpg",
+    name: 'Taylor Doe',
+    email: 'taylor@example.com',
+    role: 'seller',
+    password: '1234',
+    avatar: 'avatar.jpg',
   },
 ];
