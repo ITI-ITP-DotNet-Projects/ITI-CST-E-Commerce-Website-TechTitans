e3c1ac8776cfc0150bcc7f8d17a2158a8b892ff9
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-03 10:55:20 +0200

    feat: enhance promotions.model.js
    - Add jsDocs
    - Export an instance for promotionsModel

--- backend/models/pormotion.model.js
+++ backend/models/pormotion.model.js
@@ -1,15 +1,20 @@
-export class Promotions{
-    constructor
-    (
-        productId,
-        discount,
-        startDate,
-        endDate
-    )
-    {
-        this.productId=productId;
-        this.discount=discount;
-        this.startDate= new Date(startDate);
-        this.endDate=new Date(endDate);
-    }
+import { Model } from '../utils/model.js';
+
+export class Promotion {
+  /**
+   * @param {number} id
+   * @param {number} productId
+   * @param {number} discount
+   * @param {Date} startDate
+   * @param {Date} endDate
+   */
+  constructor(id, productId, discount, startDate, endDate) {
+    this.id = id;
+    this.productId = productId;
+    this.discount = discount;
+    this.startDate = new Date(startDate);
+    this.endDate = new Date(endDate);
+  }
 }
+
+export const promotionsModel = new Model('promotions');
