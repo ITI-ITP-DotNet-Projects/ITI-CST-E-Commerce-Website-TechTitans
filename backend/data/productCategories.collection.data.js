7e3991ce93dc829115a4c048e8d3e36441684bd5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-03 15:54:06 +0200

    feat: add productsCategories.collectoin.data.js

---
+++ backend/data/productCategories.collection.data.js
@@ -0,0 +1,26 @@
+export const productCategoriesCollectionData = [
+  {
+    id: 10110,
+    name: 'Computers & Tablets',
+  },
+  {
+    id: 10111,
+    name: 'Mobile & Accessories',
+  },
+  {
+    id: 10112,
+    name: 'TV & Home Theater',
+  },
+  {
+    id: 10113,
+    name: 'Audio & Headphones',
+  },
+  {
+    id: 10114,
+    name: 'Camera & Camcorders',
+  },
+  {
+    id: 10115,
+    name: 'Gaming Equipment',
+  },
+];
