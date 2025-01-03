429ae156f00a5608e8b664b199bea9e81aeda630
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-03 15:54:24 +0200

    feat: add products.collectoin.data.js

---
+++ backend/data/products.collection.data.js
@@ -0,0 +1,261 @@
+export const productsCollectionData = [
+  {
+    id: 10001,
+    name: 'Lenovo IdeaPad 1 Notebook',
+    price: 13500,
+    rating: {
+      avgRating: 4.3,
+      numberOfReviews: 73,
+    },
+    categoryId: 10110,
+    description: `1- Offers a full high-definition screen for crisp and clear visuals
+             2- Delivers efficient performance for everyday computing tasks
+             3- Provides smooth multitasking and enhanced performance
+             4- Ensures fast boot times and ample storage for your files and applications
+`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['Lenovo_IMG1.jpg', 'Lenovo_IMG2.jpg', 'Lenovo_IMG3.jpg'],
+    specification: {
+      brand: 'LENOVO',
+      size: '15.6',
+      weight: '4.5 lbs',
+      display: '1920x1080',
+      processor: 'Intel Core i7-12700H',
+      graphics: 'NVIDIA GeForce RTX 3060',
+      memory: '16GB DDR5',
+      storage: '1TB SSD',
+      audio: ' Harman Speakers',
+      connection: 'Wi-Fi 6, Bluetooth 5.2',
+    },
+  },
+  {
+    id: 10002,
+    name: 'Dell Inspiron 15',
+    price: 31485,
+    rating: {
+      avgRating: 4,
+      numberOfReviews: 40,
+    },
+    categoryId: 10110,
+    description: `1- What you say is nice and clear with built in dual microphones and AI that reduces background noise. With an FHD camera paired with temporal noise reduction hardware and wide dynamic range software, you'll be seen with clarity under a range of lighting conditions.
+2- Connect with power and speed with the the latest 13th Gen Intel Core processors and WiFi 6E. Want a little extra graphic muscle? Optional discreate graphics give your visuals a boost.
+3- A lid-open sensor turns on your device in a snap, and an optional fingerprint reader logs you in securely and instantly. Express Charge powers your device up to 80% in an hour, so you aren't tethered an outlet. A variety of ports, including Type-C, keep you connected to all your devices without extra dongles.
+4- Your Inspiron 15 is EPEAT Silver certified. In addition, 100% of the painted parts use low VOC waterborne paint, and the hinge cap uses bio-based materials from renewable resources. Plus, packaging trays use 100% recycled paper while other packaging components are comprised of up to 90% recycled materials.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['DELL_IMG1.jpg', 'DELL_IMG2.jpg', 'DELL_IMG3.jpg'],
+    specification: {
+      brand: 'DELL',
+      size: '14',
+      weight: '3.1 lbs',
+      display: '1920x1200',
+      processor: 'Intel Core i5-1340P',
+      graphics: 'Intel Iris Xe Graphics',
+      memory: '8GB LPDDR5',
+      storage: '512GB SSD',
+      audio: ' Waves MaxxAudio',
+      connection: 'Wi-Fi 6E, Bluetooth 5.3',
+    },
+  },
+  {
+    id: 10003,
+    name: 'HP Laptop',
+    price: 34485,
+    rating: {
+      avgRating: 2.3,
+      numberOfReviews: 100,
+    },
+    categoryId: 10110,
+    description: `1- Made for collaboration with a HP True Vision Camera with a privacy door, dual mics, and advanced noise-reduction.
+2- Face any day prepared with a dependable Intel Processor , ample storage, fast connections, and plenty of memory.
+3- This EPEAT Gold Registered and ENERGY STAR Certified device, delivers crisp quality on a 15.6" display.
+4- Using DC Dimming technology that controls brightness by adjusting direct current rather than cycling the backlight, we can eliminate screen flickering for more comfortable viewing.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['HP_IMG1.jpg', 'HP_IMG2.jpg', 'HP_IMG3.jpg'],
+    specification: {
+      brand: 'HP',
+      size: '17.3',
+      weight: '5.2 lbs',
+      display: '2560x1440',
+      processor: 'AMD Ryzen 7 7735HS',
+      graphics: 'NVIDIA GeForce RTX 4060',
+      memory: '32GB DDR5',
+      storage: '2TB SSD',
+      audio: ' Bang & Olufsen',
+      connection: 'Wi-Fi 6E, Bluetooth 5.2',
+    },
+  },
+  {
+    id: 10004,
+    name: 'Asus Vivobook Flip',
+    price: 19470,
+    rating: {
+      avgRating: 3.9,
+      numberOfReviews: 34,
+    },
+    categoryId: 10110,
+    description: `1- Unleash your creativity, knock out your next work project, or watch videos — everything’s a breeze with VivoBook Flip 14. VivoBook Flip 14 is powered by up to an Intel Core i7 processor with Intel Iris Xe Max graphics and 16 GB memory, and a large-capacity PCIe SSD gives you superfast data access.	
+    2-  With ASUS Intelligent Performance Technology, your laptop could run in high-performance status as long as you need.	
+    3- Each model in the new VivoBook series provides exceptional heat dissipation via an upgraded 8 mm heat pipe and a new IceBlade fan that efficiently accelerates heat transfer. The 87-blade IceBlade fan and impeller are made of a liquid-crystal polymer that allows them to be lighter and thinner than ordinary fans.  the IceBlade fans also use a fluid-dynamics bearing, which can achieve better vibration reduction and lower noise when compared to ordinary fans.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['ASUS_IMG1.jpg', 'ASUS_IMG2.jpg', 'ASUS_IMG3.jpg'],
+    specification: {
+      brand: 'ASUS',
+      size: '13.3',
+      weight: '2.4 lbs',
+      display: '2880x1800',
+      processor: 'Intel Core i7-1360P',
+      graphics: 'Intel Iris Xe Graphics',
+      memory: '16GB LPDDR5',
+      storage: '1TB SSD',
+      audio: ' Dolby Atmos',
+      connection: 'Wi-Fi 6E, Bluetooth 5.3',
+    },
+  },
+  {
+    id: 10005,
+    name: 'HP Spectre x360',
+    price: 74985,
+    rating: {
+      avgRating: 4.2,
+      numberOfReviews: 70,
+    },
+    categoryId: 10110,
+    description: `1- Built-in AI technology, seamless phone connectivity, and advanced security—the HP Spectre lets you do more.
+    2- Do far more, faster, on an Intel Evo Platform powered by the latest Intel Processor.
+    3- Show up with a 9MP camera, night mode, appearance filters, and AI noise reduction for seamless connectivity.
+    4- With display technology that lights each pixel separately, you'll get deeper blacks for richer contrast, enhanced color depth, and more power efficiency than traditional LED screens.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['HP2_IMG1.jpg', 'HP2_IMG2.jpg', 'HP2_IMG3.jpg'],
+    specification: {
+      brand: 'HP',
+      size: '15.6',
+      weight: '4.1 lbs',
+      display: '1920x1080',
+      processor: 'AMD Ryzen 5 7535HS',
+      graphics: 'AMD Radeon 660M Graphics',
+      memory: '8GB DDR4',
+      storage: '512GB SSD',
+      audio: ' Bang & Olufsen',
+      connection: 'Wi-Fi 6, Bluetooth 5.1',
+    },
+  },
+  {
+    id: 10006,
+    name: 'Acer Aspire A5',
+    price: 32985,
+    rating: {
+      avgRating: 3.2,
+      numberOfReviews: 20,
+    },
+    categoryId: 10110,
+    description: `1- Make light work of your daily tasks, with a full line-up of features to elevate your day-to-day—like TwinAir Cooling, a 1080p webcam, an air inlet keyboard, and aluminum cover in multiple vibrant colors.
+    2- Maximize your multitasking and productivity with the latest 13th Gen Intel Core Processors for accelerated performance. And with onboard LPDDR5 memory and M.2 SSD storage—the Aspire 5 is made to do it all.
+    3- Enjoy crisp images and sound while video calling using Acer PurifiedView and dual digital microphones with Acer PurifiedVoice. AI-powered smart solutions developed for you to collaborate confidently.
+    4- See more and do more with a 16:10 aspect ratio and narrow bezel—choose 14”, 15” or 17” 1080p IPS displays, or step it up with 1440p QHD—all equipped with Acer’s suite of visual technologies for dynamic color and viewing comfort.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['ACER_IMG1.jpg', 'ACER_IMG2.jpg', 'ACER_IMG3.jpg'],
+    specification: {
+      brand: 'ACER',
+      size: '14',
+      weight: '2.8 lbs',
+      display: '1920x1080',
+      processor: 'Intel Core i3-1215U',
+      graphics: 'Intel UHD Graphics',
+      memory: '4GB LPDDR4',
+      storage: '256GB SSD',
+      audio: ' Acer TrueHarmony',
+      connection: 'Wi-Fi 6, Bluetooth 5.0',
+    },
+  },
+  {
+    id: 10007,
+    name: 'Microsoft Surface 7',
+    price: 82485,
+    rating: {
+      avgRating: 4.1,
+      numberOfReviews: 10,
+    },
+    categoryId: 10110,
+    description: `1- Included in Surface Laptop, Copilot in Windows is an AI-powered companion that helps you work smarter, get more productive, and stay connected to the people and things in your life.
+    2- Meet your new favorite Copilot+ PC experiences to boost your productivity and unleash new creativity.
+    3- With more recycled materials, thoughtful packaging, and increased built-in efficiency, we design Surface products with the circular economy in mind. Surface Laptop is another step toward Microsoft’s goal to be carbon negative, water positive, and achieve zero waste by 2030.
+    4- Improve lighting and blur distractions in the background during video calls. When presentation matters, turn on Windows Studio Effects.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['Microsoft_IMG1.jpg', 'Microsoft_IMG2.jpg', 'Microsoft_IMG3.jpg'],
+    specification: {
+      brand: 'MICROSOFT',
+      size: '13.5',
+      weight: '2.5 lbs',
+      display: '2256x1504',
+      processor: 'Microsoft SQ3',
+      graphics: 'Microsoft SQ3 Graphics',
+      memory: '8GB LPDDR4x',
+      storage: '256GB SSD',
+      audio: ' Omnisonic Speakers',
+      connection: 'Wi-Fi 6, Bluetooth 5.1',
+    },
+  },
+  {
+    id: 10008,
+    name: 'Apple MacBook Air',
+    price: 49050,
+    rating: {
+      avgRating: 2,
+      numberOfReviews: 14,
+    },
+    categoryId: 10110,
+    description: `1- Mac is designed to be easy to use and to learn, so you can start doing what you love in no time at all.
+    2- The combination of Apple silicon and macOS delivers incredible power and speed to everything you do.
+    3- With a durable aluminum enclosure and free software updates, Mac is in it for the long haul - inside and out.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['APPLE_IMG1.jpg', 'APPLE_IMG2.jpg', 'APPLE_IMG3.jpg'],
+    specification: {
+      brand: 'APPLE',
+      size: '14',
+      weight: '3.1 lbs',
+      display: '1920x1200',
+      processor: 'Apple M2',
+      graphics: 'Apple M2 Integrated Graphics',
+      memory: '8GB Unified Memory',
+      storage: '256GB SSD',
+      audio: ' Six-speaker sound system',
+      connection: 'Wi-Fi 6, Bluetooth 5.0',
+    },
+  },
+  {
+    id: 10009,
+    name: 'Apple MacBook Pro M3 Pro',
+    price: 112470,
+    rating: {
+      avgRating: 3.7,
+      numberOfReviews: 40,
+    },
+    categoryId: 10110,
+    description: `1- MacBook Air sails through work and play — and the M3 chip brings even greater capabilities to the world’s most popular laptop. With up to 18 hours of battery life, you can take the super portable MacBook Air anywhere and blaze through whatever you’re into.
+    2- Superlight and under half an inch thin, the MacBook Air fits easily into your life and is built with the planet in mind. MacBook Air with M3 is made with 50 percent recycled materials — an Apple first. And all MacBook Air laptops have a durable recycled aluminum enclosure.
+    3- No matter how intense the workload, it stays silent thanks to a fanless design. Whether you’re multitasking between apps, editing videos in iMovie, or playing Baldur’s Gate 3 in Game Mode, the M3 chip brings more speed and fluidity to everything you do.`,
+    stock: 5,
+    sellerId: 20904,
+    images: ['APPLE2_IMG1.jpg', 'APPLE2_IMG2.jpg', 'APPLE2_IMG3.jpg'],
+    specification: {
+      brand: 'APPLE',
+      size: '16',
+      weight: '4.7 lbs',
+      display: '3024x1964',
+      processor: 'Apple M2 Pro',
+      graphics: 'Apple M2 Pro Integrated Graphics',
+      memory: '16GB Unified Memory',
+      storage: '512GB SSD',
+      audio: ' Six-speaker sound system',
+      connection: 'Wi-Fi 6, Bluetooth 5.0',
+    },
+  },
+];
