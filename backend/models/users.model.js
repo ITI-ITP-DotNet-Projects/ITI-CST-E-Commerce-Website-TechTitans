54494ec3c3249017bd6c1d28fd878ed5708e350f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-03 10:51:01 +0200

    feat: enhance users.model.js
    - Add jsDocs
    - Export an instance from usersModel

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,19 +1,20 @@
-// id: number- name: string- email: string- password: string- c: 'customer'|'seller'|'admin'- avatar: string (image path)
+import { Model } from '../utils/model.js';
 
 export class User {
-
-    constructor (
-        id,
-        name,
-        password,
-        role,
-        avatar  
-    )
-    {
-        this.id=id;
-        this.name=name;
-        this.password=password;
-        this.role=role;
-        this.avatar=avatar;
-    }
+  /**
+   * @param {number} id
+   * @param {string} name
+   * @param {string} password
+   * @param {'customer'|'seller'|'admin'} role
+   * @param {string?} avatar
+   */
+  constructor(id, name, password, role, avatar) {
+    this.id = id;
+    this.name = name;
+    this.password = password;
+    this.role = role;
+    this.avatar = avatar;
+  }
 }
+
+export const usersModel = new Model('users');
