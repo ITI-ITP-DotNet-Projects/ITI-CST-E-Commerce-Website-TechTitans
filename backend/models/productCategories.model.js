d3d7411b937ab2e51755cfe6c85439fa7e8f4bc9
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-03 10:45:09 +0200

    feat: enhance productCategories.model.js
    - Add jsDocs
    - Export an instance for productCategoriesModel

--- backend/models/productCategories.model.js
+++ backend/models/productCategories.model.js
@@ -1,14 +1,14 @@
-
-export class ProductCategory{
-    constructor
-    (
-        id,
-        name,
-        parentId
-    )
-    {
-        this.id=id;
-        this.name=name;
-        this.parentId=parentId;
-    }
+export class ProductCategory {
+  /**
+   * @param {number} id
+   * @param {string} name
+   * @param {number?} parentId
+   */
+  constructor(id, name, parentId) {
+    this.id = id;
+    this.name = name;
+    this.parentId = parentId;
+  }
 }
+
+export const productCategoriesModel = new Model('productCategories');
