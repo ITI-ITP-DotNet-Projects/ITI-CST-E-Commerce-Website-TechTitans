136ac9bd4ef74a58860f649adff92e12ec8b34d7
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-05 03:44:01 +0200

    feat: update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,6 @@
-import { usersService } from '../backend/services/users.service.js';
 import { loadData } from '../backend/utils/loadData.js';
 
 onload = async () => {
   localStorage.clear();
   loadData();
-  const newUser = await usersService.register({
-    name: 'alaa',
-    email: 'alaa2002@gmail.com',
-    password: 'alaa123',
-    role: 'Admin',
-  });
-
-  console.log('User registered successfully.');
-  console.log(newUser);
-
-  await usersService.login({
-    email: 'alaa2002@gmail.com',
-    password: 'alaa123',
-  });
-  console.log('User login successfully.');
-  console.log(await usersService.isAuthenticated());
-  console.log(await usersService.isAuthorized('admin'));
-  console.log(await usersService.isAuthorized('seller'));
-  await usersService.logout();
-  console.log(await usersService.isAuthenticated());
 };
