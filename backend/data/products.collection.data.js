266ceb17c4e5b6fe2ccb17141cdec09af5726af6
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-05 05:28:05 +0200

    chore: add new products data

--- backend/data/products.collection.data.js
+++ backend/data/products.collection.data.js
@@ -5,7 +5,7 @@ export const productsCollectionData = [
     price: 13500,
     rating: {
       avgRating: 4.3,
-      numberOfReviews: 73,
+      quantity: 73,
     },
     categoryId: 10110,
     description: `1- Offers a full high-definition screen for crisp and clear visuals
@@ -35,7 +35,7 @@ export const productsCollectionData = [
     price: 31485,
     rating: {
       avgRating: 4,
-      numberOfReviews: 40,
+      quantity: 40,
     },
     categoryId: 10110,
     description: `1- What you say is nice and clear with built in dual microphones and AI that reduces background noise. With an FHD camera paired with temporal noise reduction hardware and wide dynamic range software, you'll be seen with clarity under a range of lighting conditions.
@@ -64,7 +64,7 @@ export const productsCollectionData = [
     price: 34485,
     rating: {
       avgRating: 2.3,
-      numberOfReviews: 100,
+      quantity: 100,
     },
     categoryId: 10110,
     description: `1- Made for collaboration with a HP True Vision Camera with a privacy door, dual mics, and advanced noise-reduction.
@@ -93,7 +93,7 @@ export const productsCollectionData = [
     price: 19470,
     rating: {
       avgRating: 3.9,
-      numberOfReviews: 34,
+      quantity: 34,
     },
     categoryId: 10110,
     description: `1- Unleash your creativity, knock out your next work project, or watch videos — everything’s a breeze with VivoBook Flip 14. VivoBook Flip 14 is powered by up to an Intel Core i7 processor with Intel Iris Xe Max graphics and 16 GB memory, and a large-capacity PCIe SSD gives you superfast data access.	
@@ -121,7 +121,7 @@ export const productsCollectionData = [
     price: 74985,
     rating: {
       avgRating: 4.2,
-      numberOfReviews: 70,
+      quantity: 70,
     },
     categoryId: 10110,
     description: `1- Built-in AI technology, seamless phone connectivity, and advanced security—the HP Spectre lets you do more.
@@ -150,7 +150,7 @@ export const productsCollectionData = [
     price: 32985,
     rating: {
       avgRating: 3.2,
-      numberOfReviews: 20,
+      quantity: 20,
     },
     categoryId: 10110,
     description: `1- Make light work of your daily tasks, with a full line-up of features to elevate your day-to-day—like TwinAir Cooling, a 1080p webcam, an air inlet keyboard, and aluminum cover in multiple vibrant colors.
@@ -179,7 +179,7 @@ export const productsCollectionData = [
     price: 82485,
     rating: {
       avgRating: 4.1,
-      numberOfReviews: 10,
+      quantity: 10,
     },
     categoryId: 10110,
     description: `1- Included in Surface Laptop, Copilot in Windows is an AI-powered companion that helps you work smarter, get more productive, and stay connected to the people and things in your life.
@@ -208,7 +208,7 @@ export const productsCollectionData = [
     price: 49050,
     rating: {
       avgRating: 2,
-      numberOfReviews: 14,
+      quantity: 14,
     },
     categoryId: 10110,
     description: `1- Mac is designed to be easy to use and to learn, so you can start doing what you love in no time at all.
@@ -236,7 +236,7 @@ export const productsCollectionData = [
     price: 112470,
     rating: {
       avgRating: 3.7,
-      numberOfReviews: 40,
+      quantity: 40,
     },
     categoryId: 10110,
     description: `1- MacBook Air sails through work and play — and the M3 chip brings even greater capabilities to the world’s most popular laptop. With up to 18 hours of battery life, you can take the super portable MacBook Air anywhere and blaze through whatever you’re into.
@@ -256,6 +256,514 @@ export const productsCollectionData = [
       storage: '512GB SSD',
       audio: ' Six-speaker sound system',
       connection: 'Wi-Fi 6, Bluetooth 5.0',
+    },
+  },
+  {
+    id: 10010,
+    name: 'Apple iPhone 15 Pro Max',
+    price: 59950,
+    rating: { avgRating: 4.8, quantity: 50 },
+    categoryId: 10111,
+    description: `
+        1. 6.7-inch Super Retina XDR display with ProMotion (120Hz refresh rate).
+        2. Periscope telephoto lens with 5x optical zoom for enhanced photography.
+        3. Powered by the A17 Pro chip, delivering exceptional performance.
+        4. USB-C connectivity for universal charging standards.
+      `,
+    stock: 30,
+    sellerId: 20904,
+    images: ['Apple15_img1.jpg', 'Apple15_img2.jpg', 'Apple15_img3.jpg'],
+    specification: {
+      brand: 'Apple',
+      size: '6.7 inches',
+      weight: '240 grams',
+      display: 'Super Retina XDR OLED, 120Hz, HDR10',
+      processor: 'A17 Pro chip',
+      memory: '8GB RAM',
+      storage: '256GB',
+      battery: '4,441 mAh',
+      dimensions: '160.7 x 77.6 x 7.85 mm',
+    },
+  },
+  {
+    id: 10011,
+    name: 'Samsung Galaxy S23 Ultra',
+    price: 66950,
+    rating: { avgRating: 4.7, quantity: 40 },
+    categoryId: 10111,
+    description: `
+        1. 6.8-inch Dynamic AMOLED 2X display with 120Hz refresh rate.
+        2. 200MP wide camera for exceptional photo quality.
+        3. Powered by Snapdragon 8 Gen 2 for top-tier performance.
+        4. Integrated S Pen for enhanced productivity and creativity.
+      `,
+    stock: 25,
+    sellerId: 20904,
+    images: [
+      'Samsungs23_img1.jpg',
+      'Samsungs23_img2.jpg',
+      'Samsungs23_img3.jpg',
+    ],
+    specification: {
+      brand: 'Samsung',
+      size: '6.8 inches',
+      weight: '234 grams',
+      display: 'Dynamic AMOLED 2X, 120Hz, HDR10+',
+      processor: 'Snapdragon 8 Gen 2',
+      memory: '12GB RAM',
+      storage: '512GB',
+      battery: '5,000 mAh',
+      dimensions: '163.4 x 78.1 x 8.9 mm',
+    },
+  },
+  {
+    id: 10012,
+    name: 'XIAOMI Redmi Note 13 Pro+',
+    price: 18450,
+    rating: { avgRating: 4.5, quantity: 60 },
+    categoryId: 10111,
+    description: `
+        1. 6.67-inch AMOLED display with 120Hz refresh rate.
+        2. Powered by the Dimensity 7200 Ultra chipset for efficiency.
+        3. 200MP wide camera with OIS for high-quality photos.
+        4. 120W fast charging enables full charge in 19 minutes.
+      `,
+    stock: 40,
+    sellerId: 20904,
+    images: [
+      'Xiaomi13pro_img1.jpg',
+      'Xiaomi13pro_img2.jpg',
+      'Xiaomi13pro_img3.jpg',
+    ],
+    specification: {
+      brand: 'XIAOMI',
+      size: '6.67 inches',
+      weight: '204 grams',
+      display: 'AMOLED, 120Hz, HDR10+',
+      processor: 'Dimensity 7200 Ultra',
+      memory: '12GB RAM',
+      storage: '512GB',
+      battery: '5,000 mAh',
+      dimensions: '162.9 x 76 x 8.9 mm',
+    },
+  },
+  {
+    id: 10013,
+    name: 'INFINIX Zero Flip',
+    price: 30850,
+    rating: { avgRating: 4.3, quantity: 30 },
+    categoryId: 10111,
+    description: `
+        1. Clamshell-style foldable design with 6.9-inch AMOLED main display.
+        2. 120Hz refresh rate on the main display for smooth visuals.
+        3. Dimensity 8020 chipset ensures solid performance.
+        4. 70W Super Charge for rapid battery replenishment.
+      `,
+    stock: 20,
+    sellerId: 20904,
+    images: [
+      'Infinixflip_img1.jpg',
+      'Infinixflip_img2.jpg',
+      'Infinixflip_img3.jpg',
+    ],
+    specification: {
+      brand: 'INFINIX',
+      size: '6.9 inches (main display)',
+      weight: '195 grams',
+      display: 'AMOLED, 120Hz (main); 3.64 inches (cover)',
+      processor: 'Dimensity 8020',
+      memory: '8GB RAM',
+      storage: '512GB',
+      battery: '4,720 mAh',
+      dimensions:
+        'Unfolded: 165.5 x 75.2 x 7.6 mm; Folded: 84.5 x 75.2 x 16.2 mm',
+    },
+  },
+  {
+    id: 10014,
+    name: 'OPPO A3 PRO 5G Phone',
+    price: 15450,
+    rating: { avgRating: 4.4, quantity: 35 },
+    categoryId: 10111,
+    description: `
+        1. 6.67-inch LCD display with a 120Hz refresh rate.
+        2. Powered by Dimensity 6300 chipset for reliable performance.
+        3. 50MP main camera for quality photography.
+        4. 5100mAh battery with 45W SUPERVOOC flash charge.
+      `,
+    stock: 28,
+    sellerId: 20904,
+    images: ['OPPOA3_img1.jpg', 'OPPOA3_img2.jpg', 'OPPOA3_img3.jpg'],
+    specification: {
+      brand: 'OPPO',
+      size: '6.67 inches',
+      weight: '186 grams',
+      display: 'LCD, 120Hz',
+      processor: 'Dimensity 6300',
+      memory: '8GB RAM',
+      storage: '256GB',
+      battery: '5,100 mAh',
+      dimensions: '165.1 x 76.5 x 8.1 mm',
+    },
+  },
+
+  ,
+  {
+    id: 10015,
+    name: 'Realme 12 Pro+',
+    price: 25999,
+    rating: { avgRating: 4.4, quantity: 150 },
+    categoryId: 10111,
+    description: `
+      1. 6.7-inch AMOLED display with 120Hz refresh rate for smooth visuals.
+      2. Powered by Qualcomm Snapdragon 7S Gen 2 processor for efficient performance.
+      3. Triple camera setup with 64MP main sensor for high-quality photography.
+      4. 5000mAh battery with fast charging support for extended usage.
+    `,
+    stock: 50,
+    sellerId: 20904,
+    images: [
+      'RealMe12pro_img1.jpg',
+      'RealMe12pro_img2.jpg',
+      'RealMe12pro_img3.jpg',
+    ],
+    specification: {
+      brand: 'Realme',
+      size: '6.7 inches',
+      weight: '190 grams',
+      display: 'AMOLED, 120Hz',
+      processor: 'Qualcomm Snapdragon 7S Gen 2',
+      memory: '8GB RAM',
+      storage: '128GB',
+      battery: '5000mAh',
+      dimensions: '162.9 x 75.8 x 8.6 mm',
+    },
+  },
+  {
+    id: 10016,
+    name: 'MOTOROLA Edge 50 Ultra 5G',
+    price: 26389,
+    rating: { avgRating: 4.5, quantity: 200 },
+    categoryId: 10111,
+    description: `
+      1. 6.7-inch OLED display with high resolution for vibrant visuals.
+      2. Equipped with advanced camera system for professional-grade photos.
+      3. Premium design with high-quality materials for a sleek look.
+      4. Enhanced audio quality for an immersive experience.
+    `,
+    stock: 40,
+    sellerId: 20904,
+    images: [
+      'MotorolaEdge50_img1.jpg',
+      'MotorolaEdge50_img2.jpg',
+      'MotorolaEdge50_img3.jpg',
+    ],
+    specification: {
+      brand: 'Motorola',
+      size: '6.7 inches',
+      weight: '197 grams',
+      display: 'OLED',
+      processor: 'Qualcomm Snapdragon 8s Gen 3',
+      memory: '8GB RAM',
+      storage: '256GB',
+      battery: '4500mAh',
+      dimensions: '161.09 x 72.38 x 8.59 mm',
+    },
+  },
+  {
+    id: 10017,
+    name: 'Huawei Nova 12 SE Phone',
+    price: 24999,
+    rating: { avgRating: 4.3, quantity: 120 },
+    categoryId: 10111,
+    description: `
+      1. 6.5-inch IPS LCD display for clear and bright visuals.
+      2. Powered by Kirin 820E chipset for reliable performance.
+      3. Quad-camera setup with 64MP main sensor for versatile photography.
+      4. 4200mAh battery with fast charging support for all-day use.
+    `,
+    stock: 60,
+    sellerId: 20904,
+    images: [
+      'huaweinova12_img1.jpg',
+      'huaweinova12_img2.jpg',
+      'huaweinova12_img3.jpg',
+    ],
+    specification: {
+      brand: 'Huawei',
+      size: '6.5 inches',
+      weight: '180 grams',
+      display: 'IPS LCD',
+      processor: 'Kirin 820E',
+      memory: '8GB RAM',
+      storage: '128GB',
+      battery: '4200mAh',
+      dimensions: '160.1 x 74.1 x 7.8 mm',
+    },
+  },
+  {
+    id: 10018,
+    name: 'OnePlus Nord 4',
+    price: 21450,
+    rating: { avgRating: 4.0, quantity: 100 },
+    categoryId: 10111,
+    description: `
+      1. 6.74-inch AMOLED display with 1240 x 2772 resolution for sharp visuals.
+      2. Powered by Snapdragon 7 Plus Gen 3 processor for smooth performance.
+      3. 50MP main camera for quality photography.
+      4. Large 5500mAh battery with up to 100W wired charging for extended use.
+    `,
+    stock: 70,
+    sellerId: 20904,
+    images: [
+      'OnePlusNord_img1.jpg',
+      'OnePlusNord_img2.jpg',
+      'OnePlusNord_img3.jpg',
+    ],
+    specification: {
+      brand: 'OnePlus',
+      size: '6.74 inches',
+      weight: '189 grams',
+      display: 'AMOLED, 1240 x 2772 pixels',
+      processor: 'Snapdragon 7 Plus Gen 3',
+      memory: '12GB RAM',
+      storage: '256GB',
+      battery: '5500mAh',
+      dimensions: '160.1 x 73.8 x 8.4 mm',
+    },
+  },
+  {
+    id: 10019,
+    name: 'Google Pixel 9 Pro',
+    price: 39950,
+    rating: { avgRating: 4.6, quantity: 180 },
+    categoryId: 10111,
+    description: `
+      1. 6.7-inch OLED display with high resolution for stunning visuals.
+      2. Powered by Tensor G4 processor with enhanced AI capabilities.
+      3. 50MP main camera with advanced AI tools for exceptional photography.
+      4. Supports Satellite SOS for emergency services.
+    `,
+    stock: 30,
+    sellerId: 20904,
+    images: [
+      'GooglePixel9_img1.jpg',
+      'GooglePixel9_img2.jpg',
+      'GooglePixel9_img3.jpg',
+    ],
+    specification: {
+      brand: 'Google',
+      size: '6.7 inches',
+      weight: '199 grams',
+      display: 'OLED',
+      processor: 'Tensor G4',
+      memory: '12GB RAM',
+      storage: '256GB',
+      battery: '4700mAh',
+      dimensions: '152.8 x 72 x 8.5 mm',
+    },
+  },
+  {
+    productID: 10020,
+    ProductName: 'Landhoo 6 Pack Case for Apple Watch SE',
+    price_EGP: 1100,
+    rating: {
+      avgRating: 4.3,
+      quantity: 73,
+    },
+    CategoryID: 0,
+    Description: `1.Comprehensive Protection:
+   Made of hard PC material with a built-in tempered glass screen protector.
+   2.Ultra-Thin Design:
+  Only 0.1mm thick, maintaining the original touch sensitivity of the watch.
+  3.High Transparency:
+  The screen protector offers 99.99% clarity while preserving the original screen brightness.
+`,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['Miohhr Watch 1.jpg', 'miohhrWatch2.jpg', 'miohhrWatch3.jpg'],
+    Specs: {
+      brand: 'Landhoo',
+      size: '40mm',
+      weight: '40.8 grams',
+    },
+  },
+  {
+    productID: 10021,
+    ProductName: 'MioHHR Slim Metal Band Compatible with Apple Watch',
+    price_EGP: 1200,
+    rating: {
+      avgRating: 2.3,
+      quantity: 95,
+    },
+    CategoryID: 0,
+    Description: `1.Compatibility: The band is compatible with a range of 
+  Apple Watch models, including Series 1 through 10, as well as SE models
+
+2.Slim and Elegant Design: Featuring a narrow, waist-collecting design, the band measures only 12-14 mm in width,
+ offering a more delicate and feminine appearance compared to traditional bulky metal bands.
+
+3.Premium Stainless Steel Construction: Crafted from high-quality stainless steel, 
+this band has been refined and polished to achieve a bright, durable, and rust-resistant finish.
+
+`,
+    Stock: 0,
+    sellerId: 20904,
+    images: [
+      'MioHHR Slim Metal Band 1.jpg',
+      'MIOHHR2.jpg',
+      'MIOHHRsimMetal 3.jpg',
+    ],
+    Specs: {
+      brand: 'MioHHR',
+      size: '42mm, 44mm, 45mm',
+      weight: '40.8 grams',
+    },
+  },
+
+  {
+    productID: 10022,
+    ProductName: 'Pack Protective Case for Apple Watch',
+    price_EGP: 1000,
+    rating: {
+      avgRating: 4,
+      quantity: 70,
+    },
+    CategoryID: 0,
+    Description: `1Comprehensive Protection: Hard PC material with built-in tempered 
+    glass screen protector to guard against scratches, dust, and impacts.
+
+2.Ultra-Clear Screen: The transparent design ensures high screen clarity and 
+responsiveness, maintaining the original Apple Watch touch experience.
+3.Elegant Colors: Includes six stylish options (black, rose gold, silver, leopard, etc.)
+ to match your watch and personal style.
+  
+  
+  `,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['watch1_.jpg', 'watch2_.jpg', 'watch3.jpg'],
+    Specs: {
+      brand: 'TAURI',
+      size: '40mm',
+      weight: '45 grams',
+    },
+  },
+
+  {
+    productID: 4,
+    ProductName: 'Tasikar Band Compatible with Apple Watch ',
+    price_EGP: 900,
+    rating: {
+      avgRating: 3.5,
+      quantity: 80,
+    },
+    CategoryID: 0,
+    Description: `1. Stainless Steel Metal Band:
+    Premium Material: Crafted from durable stainless steel, providing a sleek and professional appearance
+    
+    2. Silicone Magnetic Band:
+    Soft Silicone Construction: Made from durable and soft silicone, offering a comfortable touch on the wrist.
+    
+    3. Leather-Silicone Hybrid Band:
+    Premium Materials: Combines top genuine leather with soft silicone, providing a skin-friendly and comfortable touch.
+      
+      
+      `,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['Tasikar Band 1.jpg', 'Tasikar Band2.jpg', 'Tasikar Band3.jpg'],
+    Specs: {
+      brand: 'Tasikar',
+      size: '38mm/40mm/41mm/42mm',
+      weight: '150 grams',
+    },
+  },
+
+  {
+    productID: 10023,
+    ProductName: 'Parsonver Fitness Tracke ',
+    price_EGP: 3000,
+    rating: {
+      avgRating: 4,
+      quantity: 90,
+    },
+    CategoryID: 0,
+    Description: `1-Comprehensive Fitness Tracking:
+        Tracks steps, calories burned, and distance traveled, helping you stay on top of your fitness goals.
+        
+        2- Health Monitoring:
+        Monitors heart rate in real time, providing valuable insights into your overall health.
+        
+        3-Smart Connectivity:
+        Syncs with your smartphone to display notifications for calls, messages, and social media alerts
+
+          4-Waterproof Design (IP68):
+        Designed to withstand water exposure, making it suitable for everyday use, including handwashing, rain, and swimming.
+          
+          `,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['smartWatch 1.jpg', 'smartWatch 2.jpg', 'smartwatch 3.jpg'],
+    Specs: {
+      brand: 'Parsonver',
+      size: 'Adjustable strap fits a wide range of wrist sizes',
+      weight: 'weigh between 37 grams and 40 grams',
+    },
+  },
+
+  {
+    productID: 10024,
+    ProductName: 'Xiaomi Smart Band 8',
+    price_EGP: 1.899,
+    rating: {
+      avgRating: 4.5,
+      quantity: 80,
+    },
+    CategoryID: 0,
+    Description: `1-AMOLED Touch Display: A 1.62-inch AMOLED screen with a resolution of 192 x 490 pixels, 
+    offering bright and vibrant visuals, and a smooth 60Hz refresh rate
+        
+        2-Health Monitoring: Continuous heart rate tracking, SpO2 measurement, sleep analysis, and breathing exercises.
+         These features provide in-depth insights into your health and wellness..
+        
+      3-Sport Modes and Battery Life: Over 150 sports modes to track various activities. The 190mAh battery offers 
+      up to 16 days of usage on a single charge.
+          
+          `,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['XiaomiWatch 1.jpg', 'xiamoWatch 2.jpg', 'XiaomiWatch3_.jpg'],
+    Specs: {
+      brand: 'Xiaomi',
+      size: '46.5 x 21.5 x 10.99 mm',
+      weight: ' 24.5 grams (with the band)',
+    },
+  },
+  {
+    productID: 10025,
+    ProductName: 'WHOOP 4.0 ',
+    price_EGP: 14.4,
+    rating: {
+      avgRating: 4.1,
+      quantity: 90,
+    },
+    CategoryID: 0,
+    Description: `1-Continuous Monitoring: Tracks heart rate, respiratory rate, resting heart rate, heart rate variability,
+     skin temperature, blood oxygen levels, daily activity, and sleep.
+        
+       2-Performance Optimization: Provides insights to improve sleep, manage strain, and enhance recovery
+        
+      3-Subscription Model: Requires a WHOOP membership, with options starting at $30 per month or $239 annually
+          
+          `,
+    Stock: 0,
+    sellerId: 20904,
+    images: ['WHOOP 1.jpg', 'WHOOP 2.jpg', 'WHOOP3.jpg'],
+    Specs: {
+      brand: 'WHOOP',
+      size: '41.5 x 41.5 x 11.8 mm',
+      weight: ' 13.6 grams (without the band',
     },
   },
 ];
