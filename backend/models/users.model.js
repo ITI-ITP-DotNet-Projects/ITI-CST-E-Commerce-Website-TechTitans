7d3f84707cd63f4801e93d32439d388aaebda038
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-05 03:17:43 +0200

    fix: add email field in User class

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -4,13 +4,15 @@ export class User {
   /**
    * @param {number} id
    * @param {string} name
+   * @param {string} email
    * @param {string} password
    * @param {'customer'|'seller'|'admin'} role
    * @param {string?} avatar
    */
-  constructor(id, name, password, role, avatar) {
+  constructor(id, name, email, password, role, avatar) {
     this.id = id;
     this.name = name;
+    this.email = email;
     this.password = password;
     this.role = role;
     this.avatar = avatar;
