429069bf1d53095f4460db26a4bcc8e063361230
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-09 21:07:17 +0200

    chore: add shoppingCartItems.collection.data.js

---
+++ backend/data/shoppingCartItems.collection.data.js
@@ -0,0 +1,20 @@
+export const shoppingCartItemsCollectionData = [
+  {
+    id: 20500,
+    cartId: 20403,
+    productId: 10001,
+    quantity: 1,
+  },
+  {
+    id: 20501,
+    cartId: 20403,
+    productId: 10010,
+    quantity: 2,
+  },
+  {
+    id: 20502,
+    cartId: 20403,
+    productId: 10026,
+    quantity: 3,
+  },
+];
