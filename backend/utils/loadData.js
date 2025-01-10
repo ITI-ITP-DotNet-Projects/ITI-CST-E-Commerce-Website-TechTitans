7cb783e62fb21e5d72c1e9cf6420b66d34dc3067
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-10 16:48:19 +0200

    feat: loadData.js

--- backend/utils/loadData.js
+++ backend/utils/loadData.js
@@ -6,17 +6,24 @@ import { productsModel } from '../models/products.model.js';
 import { productCategoriesModel } from '../models/productCategories.model.js';
 import { shoppingCartsModel } from '../models/shoppingCarts.model.js';
 import { shoppingCartsCollectionData } from '../data/shoppingCarts.collection.data.js';
-import {
-  shoppingCartItemsModel,
-} from '../models/shoppingCartItems.model.js';
+import { shoppingCartItemsModel } from '../models/shoppingCartItems.model.js';
 import { shoppingCartItemsCollectionData } from '../data/shoppingCartItems.collection.data.js';
+import { Model } from './model.js';
+
+/**
+ * @param {Model} model
+ */
+function try2Load(model, data) {
+  if (model.Collection.length) return;
+  model.Collection = data;
+}
 
 export function loadData() {
   console.log('start loading data...');
-  usersModel.Collection = usersCollectionData;
-  productCategoriesModel.Collection = productCategoriesCollectionData;
-  productsModel.Collection = productsCollectionData;
-  shoppingCartsModel.Collection = shoppingCartsCollectionData;
-  shoppingCartItemsModel.Collection = shoppingCartItemsCollectionData;
+  try2Load(usersModel, usersCollectionData);
+  try2Load(productsModel, productsCollectionData);
+  try2Load(productCategoriesModel, productCategoriesCollectionData);
+  try2Load(shoppingCartsModel, shoppingCartsCollectionData);
+  try2Load(shoppingCartItemsModel, shoppingCartItemsCollectionData);
   console.log('finish loading data...');
 }
