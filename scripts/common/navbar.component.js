164de283cb197c266ed055b4f9a8e4a982b3ef63
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-01-14 16:50:53 +0200

    feat: enhance navbar

--- scripts/common/navbar.component.js
+++ scripts/common/navbar.component.js
@@ -4,15 +4,63 @@ import { shoppingCartsService } from '../../backend/services/shoppingCarts.servi
 import { shoppingCartItemsService } from '../../backend/services/shoppingCartItems.service.js';
 
 /**
- * @returns {Promise<void>}
+ * @returns {string}
  */
-export async function renderNavBar() {
-  const loggedInUser = await usersService.getCurrentLoggedInUser();
-  let cartItemCount = 0;
-  let userRole = loggedInUser?.role || 'customer'; // Default role is customer if not logged in
+function getCurrentPage() {
+  return window.location.pathname.split('/').pop();
+}
+/**
+ * @param {string} authLinks
+ * @returns {Promise<string>}
+ */
+async function renderNavBarForAdmins(authLinks) {
+  return `
+      <li class="nav-item">
+          <a class="nav-link" href="./dashboard.html">Dashboard</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="./products.html">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./users.html">
+          <i class="bi bi-people"></i> Users
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./orders.html">
+          <i class="bi bi-card-checklist"></i> Orders
+        </a>
+      </li>
+      ${authLinks}`;
+}
 
-  if (loggedInUser && userRole === 'customer') {
-    // Fetch the user's shopping cart
+/**
+ * @param {string} authLinks
+ * @returns {Promise<string>}
+ */
+async function renderNavBarForSellers(authLinks) {
+  return `
+        <li class="nav-item">
+        <a class="nav-link" href="./orders.html">
+          <i class="bi bi-card-checklist"></i> Orders
+        </a>
+      </li>
+      ${authLinks}
+      <li class="nav-item">
+          <a class="nav-link" href="./contact.html">Contact</a>
+      </li>
+  `;
+}
+
+/**
+ * @param {string} authLinks
+ * @returns {Promise<string>}
+ */
+async function renderNavBarForCustomers(authLinks, loggedInUser) {
+  // Fetch the user's shopping cart
+  let cartItemCount = 0;
+
+  if (loggedInUser) {
     const [shoppingCart] = await shoppingCartsService.getShoppingCarts({
       filterOptions: { customerId: loggedInUser.id },
     });
@@ -30,6 +78,44 @@ export async function renderNavBar() {
       );
     }
   }
+
+  return `
+      <li class="nav-item">
+        <a class="nav-link" href="./home.html">Home</a>
+      </li>
+      <li class="nav-item position-relative">
+        <a class="nav-link bi bi-cart" href="./cart.html"></a>
+        ${
+          cartItemCount > 0
+            ? `<span class="badge rounded-pill bg-light position-absolute top-0 start-100 translate-middle">${cartItemCount}</span>`
+            : ''
+        }
+      </li>
+      ${authLinks}
+      <li class="nav-item">
+        <a class="nav-link" href="./contact.html">Contact</a>
+      </li>`;
+}
+
+/**
+ * @returns {Promise<void>}
+ */
+export async function renderNavBar() {
+  const loggedInUser = await usersService.getCurrentLoggedInUser();
+  let userRole = loggedInUser?.role || 'customer'; // Default role is customer if not logged in
+
+  // Conditionally render auth links
+  const authLinks = loggedInUser
+    ? `<li class="nav-item">
+         <a class="nav-link" href="./profile.html">
+           <i class="bi bi-person-circle"></i> Profile
+         </a>
+       </li>`
+    : `<li class="nav-item">
+         <a class="nav-link" href="./signin.html">
+           <i class="bi bi-person"></i> Sign In
+         </a>
+       </li>`;
 
   // Define the nav template
   const navTemplate = `
@@ -55,88 +141,25 @@ export async function renderNavBar() {
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto fw-bold fs-6">
-        <li class="nav-item">
-          <a class="nav-link {{isActiveHome}}" href="./home.html">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{isActiveProducts}}" href="./products.html">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{isActiveContact}}" href="./contact.html">Contact</a>
-        </li>
-        {{roleSpecificLinks}}
-        {{authLinks}}
+        ${await (userRole == 'customer'
+          ? renderNavBarForCustomers(authLinks, loggedInUser)
+          : userRole == 'admin'
+          ? renderNavBarForAdmins(authLinks)
+          : renderNavBarForSellers(authLinks))}
       </ul>
     </div>
   `;
 
-  // Determine the active page
-  const currentPage = window.location.pathname.split('/').pop();
-  const isActiveHome = currentPage === 'home.html' ? 'active' : '';
-  const isActiveProducts = currentPage === 'products.html' ? 'active' : '';
-  const isActiveContact = currentPage === 'contact.html' ? 'active' : '';
-  const isActiveSignin = currentPage === 'signin.html' ? 'active' : '';
-  const isActiveProfile = currentPage === 'profile.html' ? 'active' : '';
-  const isActiveUsers = currentPage === 'users.html' ? 'active' : '';
-  const isActiveOrders = currentPage === 'orders.html' ? 'active' : '';
-  const isActiveCart = currentPage === 'cart.html' ? 'active' : '';
-
-  // Conditionally render auth links
-  const authLinks = loggedInUser
-    ? `<li class="nav-item">
-         <a class="nav-link ${isActiveProfile}" href="./profile.html">
-           <i class="bi bi-person-circle"></i> Profile
-         </a>
-       </li>`
-    : `<li class="nav-item">
-         <a class="nav-link ${isActiveSignin}" href="./signin.html">
-           <i class="bi bi-person"></i> Sign In
-         </a>
-       </li>`;
-
-  // Conditionally render role-specific links
-  let roleSpecificLinks = '';
-  if (userRole === 'admin') {
-    roleSpecificLinks = `
-      <li class="nav-item">
-        <a class="nav-link ${isActiveUsers}" href="./users.html">
-          <i class="bi bi-people"></i> Users
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link ${isActiveOrders}" href="./orders.html">
-          <i class="bi bi-card-checklist"></i> Orders
-        </a>
-      </li>`;
-  } else if (userRole === 'seller') {
-    roleSpecificLinks = `
-      <li class="nav-item">
-        <a class="nav-link ${isActiveOrders}" href="./orders.html">
-          <i class="bi bi-card-checklist"></i> Orders
-        </a>
-      </li>`;
-  } else if (userRole === 'customer') {
-    roleSpecificLinks = `
-      <li class="nav-item position-relative">
-        <a class="nav-link bi bi-cart ${isActiveCart}" href="./cart.html"></a>
-        ${
-          cartItemCount > 0
-            ? `<span class="badge rounded-pill bg-light position-absolute top-0 start-100 translate-middle">${cartItemCount}</span>`
-            : ''
-        }
-      </li>`;
-  }
-
   // Render the navbar content
-  const navBarContent = renderTemplate(navTemplate, {
-    isActiveHome,
-    isActiveProducts,
-    isActiveContact,
-    authLinks,
-    roleSpecificLinks,
-    cartItemCount,
-  });
+  const navBarContent = renderTemplate(navTemplate);
 
   // Replace the content inside #mainNav
   document.getElementById('mainNav').innerHTML = navBarContent;
+
+  // Dynamically add the active class to the current page link
+  document.querySelectorAll('#mainNav .nav-link').forEach((ele) => {
+    if (ele.getAttribute('href') === `./${getCurrentPage()}`) {
+      ele.classList.add('active');
+    }
+  });
 }
